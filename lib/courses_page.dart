eab2e7af372426e40988ba1867deb8b9276a7034
ikennaumeh <kennaumeh@gmail.com>
2021-12-27 15:38:26 +0100

    update

---
+++ lib/courses_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_framework_tutorial/widgets.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+import 'courses_data.dart';
+
+class CoursesPage extends StatelessWidget {
+  const CoursesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Course> courses = Course.courses;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue[800],
+        centerTitle: true,
+        title: const AppBarTitle(),
+        leading: ResponsiveVisibility(
+          hiddenWhen: const [Condition.largerThan(name: MOBILE)],
+          child: IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.menu),
+          ),
+        ),
+        actions: [
+          const ResponsiveVisibility(
+            visible: false,
+              visibleWhen: [Condition.largerThan(name: MOBILE)],
+              child: MenuTextButton(text: 'Courses')),
+          const ResponsiveVisibility(
+              visible: false,
+              visibleWhen: [Condition.largerThan(name: MOBILE)],
+              child: MenuTextButton(text: 'About')),
+          IconButton(
+            icon: const Icon(Icons.mark_email_unread_rounded),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.logout_rounded),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(15.0),
+        children: [
+          const Center(child: PageHeader()),
+          const SizedBox(
+            height: 30,
+          ),
+          ResponsiveRowColumn(
+            rowMainAxisAlignment: MainAxisAlignment.center,
+            rowPadding: const EdgeInsets.all(30),
+            columnPadding: const EdgeInsets.all(30),
+            layout: ResponsiveWrapper.of(context).isSmallerThan(TABLET) ? ResponsiveRowColumnType.COLUMN : ResponsiveRowColumnType.ROW,
+            children: [
+              ResponsiveRowColumnItem(rowFlex: 1,child: CourseTile(course: courses[0])),
+              ResponsiveRowColumnItem(rowFlex: 1,child: CourseTile(course: courses[1])),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Center(child: SubscribeBlock()),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_framework_tutorial/courses_page.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+void main() => runApp(const AppWidget());
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget!),
+        breakpoints: const [
+          ResponsiveBreakpoint.resize(350, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(600, name: TABLET),
+          ResponsiveBreakpoint.resize(800, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(1700, name: 'XL'),
+        ],
+      ),
+      title: 'Flutter Responsive Framework',
+      home: CoursesPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
